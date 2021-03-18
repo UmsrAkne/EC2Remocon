6891839cbf00e58544e79e3ed6cfa3006cdda634
Akn <sibata486@gmail.com>
2021-03-18 21:26:09 +0900

    各コマンド実行時に、UIにログを表示するよう修正

--- EC2Remocon/ViewModels/MainWindowViewModel.cs
+++ EC2Remocon/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Prism.Mvvm;
+using System;
 using EC2Remocon.Models;
 using Prism.Commands;
 
@@ -13,6 +14,12 @@ namespace EC2Remocon.ViewModels
             set { SetProperty(ref _title, value); }
         }
 
+        private string log = "";
+        public string Log {
+            get => log;
+            set => SetProperty(ref log, value);
+        }
+
         private CLICommand CLICommand { get; } = new CLICommand();
 
         public MainWindowViewModel() {
@@ -23,7 +30,7 @@ namespace EC2Remocon.ViewModels
         public DelegateCommand StartEC2InstanceCommand {
             #region
             get => startEC2InstanceCommand ?? (startEC2InstanceCommand = new DelegateCommand(() => {
-                CLICommand.startEC2Instance();
+                Log += Environment.NewLine + CLICommand.startEC2Instance();
             }));
         }
         private DelegateCommand startEC2InstanceCommand;
@@ -33,7 +40,7 @@ namespace EC2Remocon.ViewModels
         public DelegateCommand StopEC2InstanceCommand {
             #region
             get => stopEC2InstanceCommand ?? (stopEC2InstanceCommand = new DelegateCommand(() => {
-                CLICommand.stopEC2Instance();
+                Log += Environment.NewLine + CLICommand.stopEC2Instance();
             }));
         }
         private DelegateCommand stopEC2InstanceCommand;
@@ -43,7 +50,7 @@ namespace EC2Remocon.ViewModels
         public DelegateCommand GetEC2InstanceStatusCommand {
             #region
             get => getEC2InstanceStatusCommand ?? (getEC2InstanceStatusCommand = new DelegateCommand(() => {
-                CLICommand.getEC2InstanceStatus();
+                Log += Environment.NewLine + CLICommand.getEC2InstanceStatus();
             }));
         }
         private DelegateCommand getEC2InstanceStatusCommand;
