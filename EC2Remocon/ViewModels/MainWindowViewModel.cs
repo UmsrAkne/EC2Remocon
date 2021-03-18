c079a4894952add0ee6b6ea0d89e375e6c273f84
Akn <sibata486@gmail.com>
2021-03-18 21:14:45 +0900

    EC2操作に関する各機能をUIのボタンにコマンドとしてバインディング

--- EC2Remocon/ViewModels/MainWindowViewModel.cs
+++ EC2Remocon/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Prism.Mvvm;
+using EC2Remocon.Models;
+using Prism.Commands;
 
 namespace EC2Remocon.ViewModels
 {
@@ -11,9 +13,42 @@ namespace EC2Remocon.ViewModels
             set { SetProperty(ref _title, value); }
         }
 
-        public MainWindowViewModel()
-        {
+        private CLICommand CLICommand { get; } = new CLICommand();
+
+        public MainWindowViewModel() {
 
         }
+
+
+        public DelegateCommand StartEC2InstanceCommand {
+            #region
+            get => startEC2InstanceCommand ?? (startEC2InstanceCommand = new DelegateCommand(() => {
+                CLICommand.startEC2Instance();
+            }));
+        }
+        private DelegateCommand startEC2InstanceCommand;
+        #endregion
+
+
+        public DelegateCommand StopEC2InstanceCommand {
+            #region
+            get => stopEC2InstanceCommand ?? (stopEC2InstanceCommand = new DelegateCommand(() => {
+                CLICommand.stopEC2Instance();
+            }));
+        }
+        private DelegateCommand stopEC2InstanceCommand;
+        #endregion
+
+
+        public DelegateCommand GetEC2InstanceStatusCommand {
+            #region
+            get => getEC2InstanceStatusCommand ?? (getEC2InstanceStatusCommand = new DelegateCommand(() => {
+                CLICommand.getEC2InstanceStatus();
+            }));
+        }
+        private DelegateCommand getEC2InstanceStatusCommand;
+        #endregion
+
+
     }
 }
