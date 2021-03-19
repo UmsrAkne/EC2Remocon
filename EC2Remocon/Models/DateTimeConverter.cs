37ecdbcdc5c19f4a9a1c8b51869bdd7a04b39d24
Akn <sibata486@gmail.com>
2021-03-19 20:10:10 +0900

    クラス追加 / ログの日時表示用にコンバータークラスを追加、使用

---
+++ EC2Remocon/Models/DateTimeConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace EC2Remocon.Models {
+    class DateTimeConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            DateTime dateTime = (DateTime)value;
+            return dateTime.ToString("yyyy/MM/dd HH:mm:ss");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
