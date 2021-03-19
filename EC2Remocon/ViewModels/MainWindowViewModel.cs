61912777a6704d72efb6e4afc0498b1b45db3d81
Akn <sibata486@gmail.com>
2021-03-19 19:18:33 +0900

    リネーム / プロパティ Log -> Output
    操作ログを表示する処理を追加するに当たり、Log という名称のクラスを作成する。
    （多分）衝突でエラーになったりはしないが、紛らわしいのであらかじめ名称を変更する。

--- EC2Remocon/ViewModels/MainWindowViewModel.cs
+++ EC2Remocon/ViewModels/MainWindowViewModel.cs
@@ -14,10 +14,10 @@ namespace EC2Remocon.ViewModels
             set { SetProperty(ref _title, value); }
         }
 
-        private string log = "";
-        public string Log {
-            get => log;
-            set => SetProperty(ref log, value);
+        private string output = "";
+        public string Output {
+            get => output;
+            set => SetProperty(ref output, value);
         }
 
         private CLICommand CLICommand { get; } = new CLICommand();
@@ -30,7 +30,7 @@ namespace EC2Remocon.ViewModels
         public DelegateCommand StartEC2InstanceCommand {
             #region
             get => startEC2InstanceCommand ?? (startEC2InstanceCommand = new DelegateCommand(() => {
-                Log += Environment.NewLine + CLICommand.startEC2Instance();
+                Output += Environment.NewLine + CLICommand.startEC2Instance();
             }));
         }
         private DelegateCommand startEC2InstanceCommand;
@@ -40,7 +40,7 @@ namespace EC2Remocon.ViewModels
         public DelegateCommand StopEC2InstanceCommand {
             #region
             get => stopEC2InstanceCommand ?? (stopEC2InstanceCommand = new DelegateCommand(() => {
-                Log += Environment.NewLine + CLICommand.stopEC2Instance();
+                Output += Environment.NewLine + CLICommand.stopEC2Instance();
             }));
         }
         private DelegateCommand stopEC2InstanceCommand;
@@ -50,7 +50,7 @@ namespace EC2Remocon.ViewModels
         public DelegateCommand GetEC2InstanceStatusCommand {
             #region
             get => getEC2InstanceStatusCommand ?? (getEC2InstanceStatusCommand = new DelegateCommand(() => {
-                Log += Environment.NewLine + CLICommand.getEC2InstanceStatus();
+                Output += Environment.NewLine + CLICommand.getEC2InstanceStatus();
             }));
         }
         private DelegateCommand getEC2InstanceStatusCommand;
