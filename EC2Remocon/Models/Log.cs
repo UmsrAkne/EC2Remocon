95072cff7769700a49c74073bd57bf3aa13834b2
Akn <sibata486@gmail.com>
2021-03-19 19:40:56 +0900

    クラス追加 / EC2への操作ログを表すクラスを追加

---
+++ EC2Remocon/Models/Log.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EC2Remocon.Models {
+    public class Log {
+
+        public DateTime DateTime { get; private set; } = DateTime.Now;
+
+        public String Status { get; private set; } = "";
+
+        public EC2InstanceOperation Operation { get; private set; }
+
+        public Log(EC2InstanceOperation operation, string status) {
+            Status = status;
+            Operation = operation;
+        }
+
+        public enum EC2InstanceOperation {
+            start,
+            stop,
+            statusCheck
+        }
+    }
+}
